fbbcafae80ede0c44eae4d30734ff3012a70f47a
edith007 <siddharthasthana31@gmail.com>
2020-06-04 12:00:11 +0530

    Done with making Channel, userlogged, and channelsMessages

--- application.py
+++ application.py
@@ -2,12 +2,23 @@ import os
 
 from collections import deque
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, session, request, redirect
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+
+from helpers import login_required
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
+
+# Keep track of channels Created
+channelsCreated = []
+
+# Keep track of users Logged
+usersLogged = []
+
+# Create a Dictionary
+channelsMessages = dict()
 
 
 @app.route("/")
